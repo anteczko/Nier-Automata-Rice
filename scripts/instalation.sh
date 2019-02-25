44a127f6d383dca78ae152e676ece17c7ffc53cf
anteczko <s21@onet.pl>
2019-02-25 23:43:31 +0100

    3rd instalation commit

--- scripts/instalation.sh
+++ scripts/instalation.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 echo "I'm going to install nesessary dependences"
 sudo apt-get update
-sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y
+sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev suckless-tools -y
+
+echo "Downloading and compiling i3-gaps-next"
+cd ~/Nier-Automata-Rice/
+git clone https://github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+cd ..
+sudo rm -rf i3-gaps
+
+echo "I3 was sucessfully installed!"
