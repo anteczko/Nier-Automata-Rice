c9d4deb88e81734356ab6c6fd9076eb64c35ea85
anteczko <s21@onet.pl>
2019-02-25 23:24:26 +0100

    testing instalation script

--- scripts/instalation.sh
+++ scripts/instalation.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "test!"
+echo "I'm going to install nesessary dependences"
+sudo apt-get update
+sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y
